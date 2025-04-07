4ee5a7e6e2abe6e3e28cea84dbc739e6ce643ffe
Safwan Ahmad <innoxentsayyed@gmail.com>
2025-04-07 07:06:35 +0530

    edit hero comp

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="hero-text">
         <h1>Ensure better education for a better world</h1>
         <p>
-          Our cutting-edge curriculum is designed to empower students with the
+          We Our cutting-edge curriculum is designed to empower students with the
           knowledge, skills, and experience needed to excel in the dynamic field
           of education
         </p>
